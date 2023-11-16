ada14a32aa1e85dac9867cc6acfe90a0b68731d7
구동엽 <ehd_duq94@naver.com>
2023-11-16 18:10:43 +0900

    feat : FlowLayout

---
+++ src/Chapter09/FlowLayout01.java
@@ -0,0 +1,25 @@
+package Chapter09;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayout01 extends JFrame {
+    public FlowLayout01(){
+        setTitle("FlowLayout Test");
+
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
+        c.add(new JButton("add"));
+        c.add(new JButton("sub"));
+        c.add(new JButton("mul"));
+        c.add(new JButton("div"));
+        c.add(new JButton("cal"));
+
+        setSize(300,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FlowLayout01();
+    }
+}
