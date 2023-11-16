fa61a298f252ba6dcea6dd9f2fe6b5fa0d5f3f2e
구동엽 <ehd_duq94@naver.com>
2023-11-16 18:17:10 +0900

    feat : BorderLayout

---
+++ src/Chapter09/BorderLayout01.java
@@ -0,0 +1,26 @@
+package Chapter09;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class BorderLayout01 extends JFrame {
+    public BorderLayout01(){
+        setTitle("BorderLayout Test");
+
+        Container c = getContentPane();
+
+        c.setLayout(new BorderLayout(30,20));
+        c.add(new JButton("Calculate"),BorderLayout.CENTER);
+        c.add(new JButton("add"),BorderLayout.NORTH);
+        c.add(new JButton("div"),BorderLayout.WEST);
+        c.add(new JButton("mul"),BorderLayout.EAST);
+        c.add(new JButton("sub"),BorderLayout.SOUTH);
+
+        setSize(300,200);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new BorderLayout01();
+    }
+}
