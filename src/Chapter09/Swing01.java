4bc1d47dfc141d40a9bc54a36bfb6433b529c3f6
구동엽 <ehd_duq94@naver.com>
2023-11-16 17:40:15 +0900

    feat : Swing 기본 틀 만들어 보기

---
+++ src/Chapter09/Swing01.java
@@ -0,0 +1,15 @@
+package Chapter09;
+
+import javax.swing.*;
+
+public class Swing01 extends JFrame {
+    public Swing01(){
+        setTitle("Test");
+        setSize(300,300); //300x300
+        setVisible(true); //프레임 출력
+    }
+
+    public static void main(String[] args) {
+        new Swing01();
+    }
+}
