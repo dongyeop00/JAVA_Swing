bd0c927ad96944119454dfae4c255888ba9d0969
구동엽 <ehd_duq94@naver.com>
2023-11-19 16:18:16 +0900

    feat : setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

--- src/main/Chapter09/Swing02.java
+++ src/main/Chapter09/Swing02.java
@@ -6,18 +6,19 @@ import java.awt.*;
 public class Swing02 extends JFrame {
     public Swing02(){
         setTitle("Button");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //1. 컨텐트팬 알아내기
         Container contentPane = getContentPane();
-        contentPane.setBackground(Color.ORANGE); //배경색 바꾸기
-        contentPane.setLayout(new FlowLayout()); //배치 설정
+        contentPane.setBackground(Color.ORANGE);
+        contentPane.setLayout(new FlowLayout());
 
         //2. 버튼 추가
         contentPane.add(new JButton("OK"));
         contentPane.add(new JButton("Cancle"));
         contentPane.add(new JButton("Ignore"));
 
-        setSize(300,150); //300x300
+        setSize(300,150);
         setVisible(true); //프레임 출력
     }
 
@@ -25,3 +26,4 @@ public class Swing02 extends JFrame {
         new Swing02();
     }
 }
+
