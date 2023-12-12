3b77b584472012160c3cc55b224f48232250d239
구동엽 <ehd_duq94@naver.com>
2023-12-13 02:34:38 +0900

    feat : HashMap

---
+++ src/main/Chapter07/HashMapEx.java
@@ -0,0 +1,22 @@
+package main.Chapter07;
+
+import java.util.HashMap;
+
+public class HashMapEx {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        map.put("baby","아기");
+        map.put("love","사랑");
+        map.put("apple","사과");
+
+        String kor = map.get("love");
+
+        System.out.println(kor);
+        System.out.println("size : " + map.size());
+
+        map.remove("apple");
+        System.out.println("apple 제거 완료");
+        System.out.println("size : " + map.size());
+    }
+}
